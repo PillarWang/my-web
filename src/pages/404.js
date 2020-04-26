4fffb001f3570698d9c19e7eda491247e751c137
PillarWang <pillar.wang@dscgloballlc.com>
2020-04-26 21:44:41 +0800

    prototype

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default()=>(
+    <Layout>
+        <h1>
+            OOPS, Something is wrong!
+        </h1>
+    </Layout>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,43 @@
 import React from "react"
-import {Link} from "gatsby"
+import Layout from "../components/layout"
 import Header from "../components/header"
+import {graphql, Link} from "gatsby"
 
-export default ()=>(
-    <div style={{color:`purple`}}>
-        <Link to="/contact">Contact</Link>
-        <Header headerText="Hello Gatsby!" />
-        <p>What a world.</p>
-        <img src="https://source.unsplash.com/random/400x200" alt="" />
-    </div>
+export default ({ data })=>(
+    <Layout>
+        <Header HeaderText="Amazing Pandas Eating Things" />
+        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+        {data.allMarkdownRemark.edges.map(({node})=>(
+            <div key={node.id}>
+                <Link to={node.fields.slug}>
+                    <h3>
+                        {node.frontmatter.title}{""}
+                        <span>-{node.frontmatter.date}</span>
+                    </h3>
+                    <p>{node.excerpt}</p>
+                </Link>
+            </div>
+        ))}
+    </Layout>
 )
+
+export const query = graphql`
+  query {
+    allMarkdownRemark (sort: {fields: frontmatter___date, order: DESC}){
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
